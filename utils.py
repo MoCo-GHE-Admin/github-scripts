15f27a1db6d23e5ca20bfa2420f02967fb23502f
Chris Knowles <cknowles@mozilla.com>
2021-10-20 06:02:58 -0400

    updating utils.py to the same PEP-ish style used elsewhere in this project

--- utils.py
+++ utils.py
@@ -1,13 +1,22 @@
+"""
+Helper file for code reuse throughout the github-scripts
+"""
 import os
 import toml
 
-# pat file format:
-#
-# admin = "key1"
-# read-only = "key2"
-# key99 = "key99"
-#
 def get_pat_from_file(key_name='admin'):
+    """
+    Retrieve the personal access token from a file named .gh_pat.toml
+    :param key_name: the toml key of the token in the file
+    :result: either the PAT as a string or None
+
+    pat file format:
+
+    admin = "key1"
+    read-only = "key2"
+    key99 = "key99"
+    """
+
     home = os.path.expanduser("~")
     config_file_name = ".gh_pat.toml"
     if os.path.exists(config_file_name):
@@ -15,13 +24,13 @@ def get_pat_from_file(key_name='admin'):
     elif os.path.exists(os.path.join(home, config_file_name)):
         config_file = os.path.join(home, config_file_name)
     else:
-      return None
+        return None
 
     # TODO: check that file permissions are 600
 
     try:
-      toml_blob = toml.load(config_file)
-      pat = toml_blob[key_name]
-      return pat
+        toml_blob = toml.load(config_file)
+        pat = toml_blob[key_name]
+        return pat
     except Exception:
-      return None
+        return None
