0c15d4722e2b4096dc64830173e24d83b2fdccec
Andrew Erickson <aerickson@mozilla.com>
2022-07-07 16:27:32 -0700

    use lib in old script

--- gh_org_repo_perms.py
+++ gh_org_repo_perms.py
@@ -15,10 +15,10 @@ import sys
 from getpass import getpass
 
 import alive_progress
-from github3 import exceptions as gh_exceptions
 from github3 import login
 
 import utils
+from shared import GHQuery
 
 # Roughly the number of github queries per loop.  Guessing bigger is better
 RATE_PER_LOOP = 20
@@ -83,7 +83,11 @@ def main():
         session_is_interactive = True
 
     userlist = {}
+    # function-based code
     gh_sess = login(token=args.token)
+    # object-based code
+    ghq = GHQuery()
+    ghq.init_gh_session(token=args.token)
 
     org = gh_sess.organization(args.org)
     # If a user was specified, just do that one, else, list all org members
@@ -136,61 +140,9 @@ def main():
 
     # FIXME Should I pull out "-ghsa-" repos - they NEVER find perms right.
     # Alternatively, just silently pass the NotFoundError?  (don't like that at first blush)
-    with alive_progress.alive_bar(
-        len(repolist), dual_line=True, title="getting repo permissions", force_tty=True
-    ) as bar:
-        for repo in repolist:
-            bar.text = f"  - checking {repo.name}..."
-            # print(f'DEBUG: repo: {repo.name}', file=sys.stderr
-            if repo.archived:
-                repo_name = f"*{repo.name}"
-            else:
-                repo_name = repo.name
-            try:
-                repocollabs = repo.collaborators()
-                for collaborator in repocollabs:
-                    # print(f'collab: {collaborator.login}, repo: {repo.name}, '
-                    # f'perms: {collaborator.permissions}', file=sys.stderr)
-                    # go through and update their items
-                    # External collabs aren't in the list already, so add them
-                    if args.user is None or args.user == collaborator.login:
-                        if collaborator.login not in userlist:
-                            userlist[collaborator.login] = {
-                                "role": "outside",
-                                "privpull": [],
-                                "privpush": [],
-                                "privadmin": [],
-                                "pubpull": [],
-                                "pubpush": [],
-                                "pubadmin": [],
-                            }
-                        if repo.private:
-                            if collaborator.permissions["admin"]:
-                                userlist[collaborator.login]["privadmin"].append(repo_name)
-                            if collaborator.permissions["push"]:
-                                userlist[collaborator.login]["privpush"].append(repo_name)
-                            if collaborator.permissions["pull"]:
-                                userlist[collaborator.login]["privpull"].append(repo_name)
-                        else:
-                            if collaborator.permissions["admin"]:
-                                userlist[collaborator.login]["pubadmin"].append(repo_name)
-                            if collaborator.permissions["push"]:
-                                userlist[collaborator.login]["pubpush"].append(repo_name)
-                            if collaborator.permissions["pull"]:
-                                userlist[collaborator.login]["pubpull"].append(repo_name)
-                # re: update param: print updates about quota if running interactively
-                utils.check_rate_remain(gh_sess, RATE_PER_LOOP, update=session_is_interactive)
-            except gh_exceptions.NotFoundError as err:
-                print(
-                    f"In repo {repo.name} and collab {collaborator.login} : {err.message}",
-                    file=sys.stderr,
-                )
-            except gh_exceptions.ServerError:
-                # TODO: send to stderr also?
-                print(
-                    f"50X error when processing repo: {repo_name} and collab {collaborator.login}"
-                )
-            bar()
+    userlist = ghq.update_userlist_with_permission_data(
+        userlist, repolist, user=args.user, session_is_interactive=session_is_interactive
+    )
 
     # Print The Things.
     print(
