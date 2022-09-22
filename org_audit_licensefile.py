1d42b305024df1f099349e10146ee1c30a929bcb
cknowles-moz <cknowles@mozilla.com>
2022-09-21 21:13:46 -0400

    Updates to audit_licensefile (#66)
    * Update output strategy for audit, add activity indicator, obey rate limits
    
    * Updating audit_licensefile to include dates, and html links.

--- org_audit_licensefile.py
+++ org_audit_licensefile.py
@@ -5,6 +5,8 @@ Script to perform a search of supplied orgs, and return the list of detected rep
 
 import argparse
 import configparser
+import sys
+from datetime import datetime
 from getpass import getpass
 
 from github3 import exceptions as gh_exceptions
@@ -34,6 +36,12 @@ def parse_arguments():
         choices=["public", "private", "all"],
     )
     parser.add_argument(
+        "--include-URL",
+        dest="url",
+        action="store_true",
+        help="Include the URL to the repo as a help for people analyzing things",
+    )
+    parser.add_argument(
         "--orgini",
         help='use "orglist.ini" with the "orgs" ' "entry with a csv list of all orgs to check",
         action="store_const",
@@ -55,12 +63,24 @@ def parse_arguments():
     return args
 
 
+def munge_date(date_str):
+    """
+    Given the repolist created date string, give me a yyyy-mm-dd string
+    param: date_str = 2021-07-06T15:04:04Z
+    return: a string - 2021-07-06
+    """
+    format_str = "%Y-%m-%dT%H:%M:%SZ"
+    date = datetime.strptime(date_str, format_str)
+    return date.date().isoformat()
+
+
 def main():
     """
     Taking in the query and list of orgs, run the search,
     print out the org name and the list of repos affected.
     """
     args = parse_arguments()
+    resultlist = []
     # Read in the config if there is one
     orglist = []
     if args.orgini is not None:
@@ -71,7 +91,16 @@ def main():
         orglist = args.orgs
 
     gh_sess = login(token=args.token)
-    print("org/repo, detected license file, license type")
+    linedict = {
+        "org": "Org",
+        "repo": "Repo",
+        "created": "Created Date",
+        "file": "Detected license file",
+        "type": "License type",
+    }
+    if args.url:
+        linedict["url"] = "URL"
+    resultlist.append(linedict)
     for orgname in orglist:
         org = gh_sess.organization(orgname)
         # Get the list of repos
@@ -84,13 +113,36 @@ def main():
         else:
             raise Exception(f"{args.type} not a known repository visibility type")
         for repo in repolist:
+            datestr = munge_date(repo.created_at)
             if (repo.archived and args.archived) or not repo.archived:
                 try:
                     license = repo.license()
                 except gh_exceptions.NotFoundError:
-                    print(f"{repo.owner}/{repo.name},NO LICENSE DETECTED")
+                    linedict = {
+                        "org": f"{repo.owner}",
+                        "repo": f"{repo.name}",
+                        "created": datestr,
+                        "file": "",
+                        "type": "NO LICENSE DETECTED",
+                    }
                 else:
-                    print(f"{repo.owner}/{repo.name},{license.name},{license.license.name}")
+                    linedict = {
+                        "org": f"{repo.owner}",
+                        "repo": f"{repo.name}",
+                        "created": datestr,
+                        "file": license.name,
+                        "type": license.license.name,
+                    }
+                if args.url:
+                    linedict["url"] = f"{repo.html_url}"
+                resultlist.append(linedict)
+            utils.spinner()
+            utils.check_rate_remain(gh_sess)
+    # Time to print things
+    print(file=sys.stderr)
+
+    for line in resultlist:
+        print(",".join(line.values()))
 
 
 if __name__ == "__main__":
