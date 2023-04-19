855a428840875186db582e03e3ec1ba3a8dd01ac
Chris Knowles <cknowles@mozilla.com>
2023-04-19 15:28:32 -0400

    Adding no-wiki flag, as well as 'is this a fork'

--- repo_activity.py
+++ repo_activity.py
@@ -128,13 +128,13 @@ def mini_repo_activity(gh_sess, orgstr, repostr, token, issues, ignore_wiki, bar
             result_string = (
                 f"{orgstr}/{repo.name}{issue_whacky},{repo.created_at.strftime('%Y-%m-%d')},"
                 f"{pushed_date.strftime('%Y-%m-%d')},{repo.updated_at.strftime('%Y-%m-%d')},"
-                f"{repo.private},{repo.archived},{issuecount}"
+                f"{repo.private},{repo.archived},{repo.fork},{issuecount}"
             )
         else:
             result_string = (
                 f"{orgstr}/{repo.name}{issue_whacky},{repo.created_at.strftime('%Y-%m-%d')},"
                 f"{pushed_date.strftime('%Y-%m-%d')},{repo.updated_at.strftime('%Y-%m-%d')},"
-                f"{repo.private},{repo.archived}"
+                f"{repo.private},{repo.archived},{repo.fork}"
             )
     except gh_exceptions.ConnectionError:
         print(f"Timeout error, 'CLOUD' on repo {orgstr}/{repostr}", file=sys.stderr)
@@ -162,14 +162,14 @@ def main():
     # Print out the header.
     if args.issues:
         output_str.append(
-            "Org/Repo, Created, Updated, Admin_update, Private, Archive_status, Issue_Count"
+            "Org/Repo,Created,Updated,Admin_update,Private,Archive_status,IsFork,Issue_Count"
         )
     else:
-        output_str.append("Org/Repo, Created, Updated, Admin_update, Private, Archive_status")
+        output_str.append("Org/Repo,Created,Updated,Admin_update,Private,Archive_status,IsFork")
 
     with alive_progress.alive_bar(
         dual_line=True,
-        title="Getting Perms",
+        title="Getting activity",
         file=sys.stderr,
         length=20,
         force_tty=True,
